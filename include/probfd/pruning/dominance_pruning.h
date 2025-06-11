642ca760f5addc97ba8bbfe5308be656ac290510
Rasmus Tollund <rgt@cs.aau.dk>
2025-06-11 16:41:01 +0200

    remove print

--- include/probfd/pruning/dominance_pruning.h
+++ include/probfd/pruning/dominance_pruning.h
@@ -42,7 +42,7 @@ public:
                         auto target_state = state_space.get_state(target.item);
                         return dominance_relation->dominates(source_state, target_state);
                     })) {
-                std::println("Pruned transition {} --{}--> because it has a target state that is dominated by the source state.", source_state.get_id().get_value(), action_name(source_state.get_task(), tail.action));
+                // std::println("Pruned transition {} --{}--> because it has a target state that is dominated by the source state.", source_state.get_id().get_value(), action_name(source_state.get_task(), tail.action));
                 ++num_transitions_pruned;
                 return true; // Prune this transition
             }
@@ -53,28 +53,28 @@ public:
                     return false; // Skip the current tail
                 }
                 // Dominates if it has at least one target and all targets dominate a target of tail
-                std::println("Checking other transition {} --{}-->", source_state.get_id().get_value(), action_name(source_state.get_task(), other_tail.action));
+                // std::println("Checking other transition {} --{}-->", source_state.get_id().get_value(), action_name(source_state.get_task(), other_tail.action));
                 bool dominates = !other_tail.successor_dist.non_source_successor_dist.empty() && std::ranges::all_of(
                     other_tail.successor_dist.non_source_successor_dist,
                     [&](const ItemProbabilityPair<StateID>& other_target_state_pair) {
                         const auto other_target_state = state_space.get_state(other_target_state_pair.item);
 
-                        std::println("Checking target {}", other_target_state.get_id().get_value());
+                        // std::println("Checking target {}", other_target_state.get_id().get_value());
                         return std::ranges::any_of(
                             tail.successor_dist.non_source_successor_dist,
                             [&](const ItemProbabilityPair<StateID>& this_target_state_pair) {
                                 const auto this_targe_state = state_space.get_state(this_target_state_pair.item);
 
                                 bool res = dominance_relation->dominates( other_target_state, this_targe_state);
-                                if (res) {
-                                    std::println("Target {} dominates {}", this_targe_state.get_id().get_value(), other_target_state.get_id().get_value());
-                                }
+                                // if (res) {
+                                //     std::println("Target {} dominates {}", this_targe_state.get_id().get_value(), other_target_state.get_id().get_value());
+                                // }
                                 return res;
                             });
                     });
-                if (dominates) {
-                    std::println("Pruned transition {} --{}--> because it is dominated by {}", source_state.get_id().get_value(), action_name(source_state.get_task(), tail.action), action_name(source_state.get_task(), other_tail.action));
-                }
+                // if (dominates) {
+                //     std::println("Pruned transition {} --{}--> because it is dominated by {}", source_state.get_id().get_value(), action_name(source_state.get_task(), tail.action), action_name(source_state.get_task(), other_tail.action));
+                // }
                 return dominates;
             })) {
                 ++num_transitions_pruned;
@@ -93,7 +93,7 @@ public:
             // The max-player will never choose this outcome, because the other is worse
             for (const auto& [other_state_id, _] : distribution) {
                 if (target_state.item != other_state_id && dominance_relation->dominates(state_space.get_state(target_state.item), state_space.get_state(other_state_id))) {
-                    std::println("Pruned outcome {} because it dominates {}", target_state.item.id, other_state_id.id);
+                    // std::println("Pruned outcome {} because it dominates {}", target_state.item.id, other_state_id.id);
                     ++num_outcomes_pruned;
                     return true;
                 }
